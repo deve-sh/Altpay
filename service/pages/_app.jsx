77c8b78c6cd96159d0fb49ce53c7bb9218df8d22
Devesh Kumar <devesh2027@gmail.com>
2022-06-30 20:39:37 +0530

    feat: Added alignment fixes to Button comp

--- service/pages/_app.jsx
+++ service/pages/_app.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import Authentication from "../components/Auth";
 import GlobalStyles from "../components/common/GlobalStyles";
 
 function Alt({ Component: Page, pageProps }) {
+	const [showAuth, setShowAuthModal] = useState(false);
+
+	const toggleAuthModal = () => setShowAuthModal((show) => !show);
+	const closeAuthModal = () => setShowAuthModal(false);
+
 	return (
 		<>
 			<GlobalStyles />
-			<Page {...pageProps} />
+			<Authentication show={showAuth} closeAuthModal={closeAuthModal} />
+			<Page {...pageProps} toggleAuthModal={toggleAuthModal} />
 		</>
 	);
 }
