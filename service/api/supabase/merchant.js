ffd2f3780e802308b54af102d8fa00ab4a75add7
Devesh Kumar <devesh2027@gmail.com>
2022-07-02 14:34:48 +0530

    feat: Added merchant related API database functions

---
+++ service/api/supabase/merchant.js
@@ -0,0 +1,20 @@
+import supabase from "./index";
+
+export const getMerchant = (merchantId) =>
+	supabase.from("merchants").select().eq("id", merchantId).single();
+
+export const getMerchantQRInfo = (qrLink, merchantId) => {
+	try {
+		const upiIdFromQRLink = new URLSearchParams(
+			new URL(qrLink.replace("upi://", "https://")).search
+		).get("pa");
+		return supabase
+			.from("merchant_upi_ids_and_qrs")
+			.select()
+			.eq("merchant_id", merchantId)
+			.eq("upi_id", upiIdFromQRLink)
+			.single();
+	} catch (err) {
+		return { data: null, error: err.message };
+	}
+};
