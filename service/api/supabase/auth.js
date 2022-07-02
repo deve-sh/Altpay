42e4cb82c290fddbda2188e6174e052473c27d3e
Devesh Kumar <devesh2027@gmail.com>
2022-07-02 13:53:55 +0530

    feat: Added Redirect back to start point for Login

--- service/api/supabase/auth.js
+++ service/api/supabase/auth.js
@@ -1,6 +1,9 @@
 import supabase from "./index";
 
 export const signInWithGoogle = () =>
-	supabase.auth.signIn({
-		provider: "google",
-	});
+	supabase.auth.signIn(
+		{
+			provider: "google",
+		},
+		{ redirectTo: window.location.toString() }
+	);
